0675498f2af23230c9451e5bac888b4297235808
kwamebrowne <theeecryptoking@gmail.com>
2023-04-15 19:15:35 -0500

    .reduce unlocked

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
-// Code your solution here
+// Learning Goals
+// Learn how the reduce() method works
+// Demonstrate using reduce() with a primitive return value
+// Demonstrate using reduce() with an object as the return value
+// Create a new variable called totalBatteries, which holds the sum of all of the battery amounts in the batteryBatches array. (Note that the batteryBatches variable has been provided for you in index.js.) Naturally, you should use reduce() for this!
+// reduce() method is used when we want to get some information from each element in the collection and gather that information into a final summary value.
+let totalBatteries = batteryBatches.reduce(function (totaler, amounts) {
+    return totaler += amounts
+}, 0)
